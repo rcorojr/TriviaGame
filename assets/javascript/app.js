cabe33b3b73a5cbae501a467d55ee59239e5c802
Rey Coronado <rcorojr@gmail.com>
2020-05-07 17:21:05 -0600

    functioning timer set up.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,8 +80,20 @@ function startQuiz(quizField, quizQuest, submitButton, resultsBox){
 }
 
 //Timer
-const quizTime =  100
 
-function countRun(){
+var count=60;
 
+var counter=setInterval(timer, 1000);
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     return;
+  }  
+  document.getElementById("timer").innerHTML=count;
 }
+
+
