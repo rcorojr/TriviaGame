b6db9da2de635263f46e7e89ac8a60947e1b3be2
Rey Coronado <rcorojr@gmail.com>
2020-05-07 16:30:35 -0600

    add basic JS structure for timer, questions, and quiz initiator.

---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+//Array for questions
+let quizQuest = [
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+    {
+        question : "How many legs does the Legs of Man have?",
+        opt1 : "3",
+        opt2 : "1",
+        opt3 : "2",
+        correct : "3"
+    },
+]
+
+// function to initiate quiz
+function startQuiz(quizField, quizQuest, submitButton, resultsBox){
+    function viewQuest(quizQuestions, quizField){
+
+    }
+}
+
+//Timer
+const quizTime =  100
+
+function countRun(){
+
+}
