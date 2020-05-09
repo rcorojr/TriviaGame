a1f6ef502e322d41067a4a162057532b8e7f99e2
Rey Coronado <rcorojr@gmail.com>
2020-05-09 16:55:31 -0600

    adding checkboxes with single checkbox.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -76,10 +76,13 @@ var lastQuestionIndex = quizQuest.length - 1;
 function viewQuest(quizQuest, runningQuestionIndex){
     var q = quizQuest[runningQuestionIndex];
     var questionelement = $("#quiz");
+    var opt1=$(`<label>${q.opt1}</label><input type='checkbox' data-questionindex=${runningQuestionIndex} data-option=${q.opt1}>`);
+    var opt2=$(`<label>${q.opt2}</label><input type='checkbox' data-questionindex=${runningQuestionIndex} data-option=${q.opt2}>`);
+    var opt3=$(`<label>${q.opt3}</label><input type='checkbox' data-questionindex=${runningQuestionIndex} data-option=${q.opt3}>`);
     questionelement.append("<p>" + q.question + "</p>");
-    opt1.innerHTML = q.opt1;
-    opt2.innerHTML = q.opt2;
-    opt3.innerHTML = q.opt3;
+    questionelement.append(opt1);
+    questionelement.append(opt2);
+    questionelement.append(opt3);
   }
 
 for (i=0; i<quizQuest.length; i++){
@@ -90,22 +93,27 @@ for (i=0; i<quizQuest.length; i++){
 
 var count=200;
 var timerInterval=null;
-// var counter=setInterval(timer, 1000);
+$("input").on("click", function(){
+    var currentclickboxindex = $(this).attr("data-questionindex");
+    console.log(currentclickboxindex)
+    Array.from($("input")).forEach(function(checkbox){
+        if ($(checkbox).attr("data-questionindex")==currentclickboxindex
 
-// document.getElementById("startBtn").addEventListener("click", timer)
+        ){
+            console.log("foundmatches")
+            $(checkbox).prop("checked", false)
+        }
+    })
+    $(this).prop("checked", true);
+    
+})
+
+
 document.getElementById("startBtn").addEventListener("click", function (){
     if (timerInterval) clearInterval(timerInterval);
     timerInterval = setInterval(timer, 1000)});
 
-// document.getElementById("startBtn").addEventListener("click", function() {
-//     count=count-1;
-//     if (count <= 0)
-//     {
-//        clearInterval(counter);
-//        return;
-//     }  
-//     document.getElementById("timer").innerHTML=count;
-// })
+
 
 function timer()
 {
